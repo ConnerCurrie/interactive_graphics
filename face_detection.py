87ced26d60fa69d0caa8a183801005e5b977c071
Conner Currie <connercurrie@Conners-MacBook-Pro.local>
2020-09-07 12:31:33 -0700

    Added code to integrate face detection into pygame loop

--- face_detection.py
+++ face_detection.py
@@ -5,50 +5,38 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
+#Import cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-    #Import cascades
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+#Define video capture object
+vc = cv2.VideoCapture(0)
 
-    #Define video capture object
-    vc = cv2.VideoCapture(0)
+#Image capture loop
+if vc.isOpened(): # try to get the first frame
+    is_capturing, frame = vc.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)    # makes the blues image look real colored
+    webcam_preview = plt.imshow(frame)    
+else:
+    is_capturing = False 
 
-    #Image capture loop
-    if vc.isOpened(): # try to get the first frame
-        is_capturing, frame = vc.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)    # makes the blues image look real colored
-        webcam_preview = plt.imshow(frame)    
-    else:
-        is_capturing = False 
+def get_face_location():
 
-    while is_capturing:
-        try:    # Lookout for a keyboardInterrupt to stop the script
-            is_capturing, frame = vc.read()
-            #Convert colours
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            try:
-                #Detect faces
-                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-            except:
-                pass #no faces
-            else: 
-                #Add ROI rectangles
-                for (x,y,w,h) in faces:
-                    # frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-                    print("X coordinate: " + str(x))
-                    print("Y coordinate: " + str(y))
-                    # roi_gray = gray[y:y+h, x:x+w]
-                    # roi_color = frame[y:y+h, x:x+w]
-                    # eyes = eye_cascade.detectMultiScale(roi_gray)
-                    
-                    # for (ex,ey,ew,eh) in eyes:
-                    #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            
-            #Output to plot
-            # plt.imshow(frame)
-            # plt.show()
-            
-        except KeyboardInterrupt:
-            vc.release()
+    is_capturing, frame = vc.read()
+    #Convert colours
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    try:
+        #Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    except:
+        return #no faces
+    else: 
+        #Add ROI rectangles
+        for (x,y,w,h) in faces:
+            # frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+            # print("X coordinate: " + str(x))
+            # print("Y coordinate: " + str(y))
+
+            return x, y
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from OpenGL.GLU import *
 # import OpenGL.GLUT as GLUT
 import pygame
 from pygame.locals import *
+import face_detection
 
 print("Imports successful!")
-
 
 cubeVertices = ((1,1,1),(1,1,-1),(1,-1,-1),(1,-1,1),(-1,1,1),(-1,-1,-1),(-1,-1,1),(-1,1,-1))
 cubeEdges = ((0,1),(0,3),(0,4),(1,2),(1,7),(2,5),(2,3),(3,6),(4,6),(4,7),(5,6),(5,7))
@@ -45,13 +45,33 @@ if __name__ == "__main__":
 
     glTranslatef(0.0,0.0, -5)
 
+    x = 0
+    y = 0
+    theta_x = 0
+    theta_y = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+        #If both are None Types
+        try:
+            temp_x, temp_y = face_detection.get_face_location()
+        except:
+            continue
+
+        if temp_x != None:
+            x = 45*(temp_x - 500)/500
+        if temp_y != None:
+            y = -1*45*(temp_y - 175)/175
+
+        glRotatef(x - theta_x, 0, 1, 0)
+        glRotatef(y - theta_y, 1, 0, 0)
+        theta_y = y
+        theta_x = x
+
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         solidCube()
         wireCube()
